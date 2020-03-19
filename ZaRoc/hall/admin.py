60fa2dadcf23472428da714bbe302f636669baed
rahulsunil2 <rahulsunil2@gmail.com>
2020-03-19 13:48:03 +0530

    Change in ForeignKey onDelete

--- ZaRoc/hall/admin.py
+++ ZaRoc/hall/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Booking, Faculty, Hall
 
-# Register your models here.
+admin.site.register(Booking)
+admin.site.register(Faculty)
+admin.site.register(Hall)
--- ZaRoc/hall/models.py
+++ ZaRoc/hall/models.py
@@ -1,22 +1,32 @@
 from django.db import models
 
 class Faculty(models.Model):
-    id = models.DecimalField(unique=True)
+    id = models.DecimalField(primary_key=True, decimal_places=0, max_digits=10)
     name = models.CharField(max_length=60)
     dept =  models.CharField(max_length=60)
-    auth_level = models.DecimalField(max_digits=1)
+    auth_level = models.DecimalField(max_digits=1, decimal_places=0)
 
-class Halls(models.Model):
+    class Meta:
+        verbose_name_plural = "Faculties"
+
+class Hall(models.Model):
     no = models.CharField(max_length=4, unique=True)
     name = models.CharField(max_length=60)
-    capacity = models.DecimalField(max_digits=3)
-    inCharge = models.ForeignKey(Faculty, models.SET_NULL, null=True)
+    capacity = models.DecimalField(max_digits=3, decimal_places=0)
+    inCharge = models.ForeignKey(Faculty, models.PROTECT)
 
-class Bookings(models.Model):
-    bId = models.DecimalField(unique=True)
+    class Meta:
+        verbose_name_plural = "Halls"
+
+class Booking(models.Model):
+    bId = models.DecimalField(
+        primary_key=True, decimal_places=0, max_digits=10)
     sTime = models.DateTimeField('Start Time')
     eTime = models.DateTimeField('End Time')
-    hallNo = models.ForeignKey(Halls, models.CASCADE)
+    hallNo = models.ForeignKey(Hall, models.CASCADE)
     fId = models.ForeignKey(Faculty, models.SET_NULL, null=True)
     eventName = models.CharField(max_length=60)
     eventDetails = models.TextField('Event Details', blank=True)
+
+    class Meta:
+        verbose_name_plural = "Bookings"
