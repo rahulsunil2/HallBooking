84e635966225f3714d96d2e072902833904f38ee
5hade5layer <bharath218usba@gmail.com>
2020-04-22 13:32:19 +0530

    date feild lable added, book button named

--- ZaRoc/hall/forms.py
+++ ZaRoc/hall/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class searchbar(forms.Form):
     searchbar = forms.CharField(label='Search', max_length=100)
 class DateForm(forms.Form):
-    date = forms.DateField(input_formats=['%d/%m/%Y'])
+    date = forms.DateField(input_formats=['%d/%m/%Y %H:%M'])
